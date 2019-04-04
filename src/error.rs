a46b1214ee4bdc27f3c6175355dd835afe744205
Markus Kohlhase <markus.kohlhase@slowtec.de>
2019-04-04 16:30:35 +0200

    impl Display for Error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 /// modbus-core Error
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Error {
@@ -7,4 +9,16 @@ pub enum Error {
     BufferSize,
     /// Invalid function code
     FnCode(u8),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Error::*;
+
+        match self {
+            CoilValue(v) => write!(f, "Invalid coil value: {}", v),
+            BufferSize => write!(f, "Invalid buffer size"),
+            FnCode(fn_code) => write!(f, "Invalid function code: 0x{:0>2X}", fn_code),
+        }
+    }
 }
